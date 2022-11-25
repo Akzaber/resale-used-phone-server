4b3906067bbf8ff5a7130b77f3b2b624ee5ecc83
Akzaber <zaberkhan611@gmail.com>
2022-11-26 03:45:30 +0600

    dynamic id api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const app = express();
@@ -27,6 +27,9 @@ async function run() {
     const usedOneplusCollection = client
       .db("resalePhone")
       .collection("usedOneplus");
+    const usedMobileCollection = client
+      .db("resalePhone")
+      .collection("usedPhoneCollections");
 
     app.get("/iphoneCollection", async (req, res) => {
       const query = {};
@@ -43,6 +46,19 @@ async function run() {
     app.get("/onepluscollection", async (req, res) => {
       const query = {};
       const result = await usedOneplusCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/usedphonecategory", async (req, res) => {
+      const query = {};
+      const result = await usedMobileCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/usedphonecategory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await usedMobileCollection.find(query).toArray();
       res.send(result);
     });
   } finally {
